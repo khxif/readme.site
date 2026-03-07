27cce0ff3723189ded9388110ef63a40c81a9ee1
Khaif Mohamed <khaifmohamededava@gmail.com>
2026-03-07 08:16:12 +0530

    chore: initial setup

---
+++ client/src/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return <main className="flex items-center justify-center w-full h-svh">{children}</main>;
+}
