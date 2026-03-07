04ea7a14c74d9d4112e5b60d0dd62cd3f08adbc8
Khaif Mohamed <khaifmohamededava@gmail.com>
2026-03-07 09:43:40 +0530

    feat: implemented auth

--- client/src/app/(auth)/auth/login/page.tsx
+++ client/src/app/(auth)/auth/login/page.tsx
@@ -1,3 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { supabase } from '@/supabase/client';
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  async function handleLogin() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <Button onClick={handleLogin}>Login</Button>
+    </div>
+  );
 }
